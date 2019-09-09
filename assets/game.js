28b75e21817e44993bfad75a15a8f571629f86d4
Jordan Ottaway <jtottaway@gmail.com>
2019-09-08 22:38:04 -0600

    images added

---
+++ assets/game.js
@@ -0,0 +1,13 @@
+// <!--p/ you will be given a random number at begining of game -->
+// <!--p/ there are 4 crystals below. by clicking on a crystal you will add a specific amount of points to your total score. -->
+// <!--p/ you will win the game by matching your total score to the random number, you lose the game if your total score goes above the random number. -->
+// <!--p/ the value of each crystal is hidden from you till you click on it. -->
+// <!--p/ each time the game starts, the game will change the values of each crystal. -->
+
+// <!-- random number display      &&    wins and losses counter -->
+
+// <!-- four crystal buttons -->
+
+// <!-- p/total score -->
+
+// <!-- total score display -->
