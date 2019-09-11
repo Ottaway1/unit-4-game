d0f9146d86770cd2a812616c2c59e835eb2c24b7
Jordan Ottaway <jtottaway@gmail.com>
2019-09-10 18:04:25 -0600

    html and css finished Jquery started

--- assets/game.js
+++ assets/game.js
@@ -1,8 +1,11 @@
 // <!--p/ you will be given a random number at begining of game -->
+
+
+
 // variables to define: random number, value of each crystal, wins, losses, user total score
-var randomNumber = Math.floor(Math.random() * 120) + 19;
+
 // Math.floor(Math.random() * 100) + 1; // returns a random integer from 1 to 100;
-$("#random").append(randomNumber);
+
 // 
 // <!--p/ there are 4 crystals below. by clicking on a crystal you will add a specific amount of points to your total score. -->
 // buttons for each crystal//
@@ -19,3 +22,66 @@ $("#random").append(randomNumber);
 // <!-- p/total score -->
 
 // <!-- total score display -->
+
+$(document).ready(function () {
+    // all custom jQuery will go here
+
+
+    var diamondOutput = Math.floor((Math.random() * 12) + 1);
+    var rudyOutput = Math.floor((Math.random() * 12) + 1);
+    var topazOutput = Math.floor((Math.random() * 12) + 1);
+    var emeraldOutput = Math.floor((Math.random() * 12) + 1);
+
+
+    var winsOutput = 0;
+    var lossesOutput = 0;
+    var userScore = {};
+
+    
+    var randomNumber = Math.floor((Math.random() * 120) + 19);
+    
+   
+   
+    $(document).ready( function () {
+        $("#random-number").append(randomNumber);
+    })
+    
+    
+    
+    
+    
+    
+    
+    $(document).on("click", "#diamond", function () {
+        $("#score").append(diamondOutput+ userScore);
+    })
+
+    $(document).on("click", "#ruby", function () {
+        alert("test");
+    })
+    $(document).on("click", "#topaz", function () {
+        alert("test");
+    })
+    $(document).on("click", "#emerald", function () {
+        ;
+    })
+
+
+
+
+
+    function reset() {
+        ;
+      }
+
+
+
+
+
+});
+
+
+
+
+
+
